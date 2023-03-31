4cf17149e42a063da3dc00ceae9d14edb186f020
Anh Tran Le Tuan <anh.letuan.tran.computer@gmail.com>
2023-03-31 11:21:44 -0500

    second commit

--- src/main/java/example/personalproject/course/CourseRepository.java
+++ src/main/java/example/personalproject/course/CourseRepository.java
@@ -1,11 +1,12 @@
 package example.personalproject.course;
 
-import java.util.List; 
-
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /* From extending the CrudRepository interface, Spring Boot will also help us to
 implement these functions, allowing us to interact with the database */
+@Repository
 public interface CourseRepository extends CrudRepository<Course, String>{
 	
 	public List<Course> findByTopicName(String name);
--- src/main/java/example/personalproject/topic/TopicRepository.java
+++ src/main/java/example/personalproject/topic/TopicRepository.java
@@ -1,9 +1,11 @@
 package example.personalproject.topic;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /* From extending the CrudRepository interface, Spring Boot will also help us to
  implement these functions, allowing us to interact with the database */
+@Repository
 public interface TopicRepository extends CrudRepository<Topic, String>{
 	
 
